22b8e0ea21909618233ebe2f8fd84aac463062e7
Farhad Rahimov <farhad.r2006@mail.ru>
2025-12-16 01:40:09 +0300

    frontend changed

--- src/main/java/com/example/Job_Post/service/EmailService.java
+++ src/main/java/com/example/Job_Post/service/EmailService.java
@@ -19,7 +19,7 @@ public class EmailService {
 
     // private static final String FRONTEND_URL = "http://localhost:5173";
     private static final String FRONTEND_URL = "https://kontral.onrender.com";
-    private static final String BACKEND_URL = "http://localhost:8080";
+    private static final String BACKEND_URL = "https://kontral-backend.onrender.com";
 
     public void sendVerificationEmail(String email, String verificationToken) {
         String subject = "Email Verification";
